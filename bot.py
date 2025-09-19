c5707a0e99303f456a1aa90f696e86c478b83a85
DaniilprogerAI <daniilprogerai@gmail.com>
2025-09-19 10:30:54 +0300

    Create file bot.py for basic code

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Бот {bot.user} запущен и готов!")
+
+@bot.command()
+async def submit(ctx, *, текст):
+    await ctx.send(f"{ctx.author.mention} отправил задачу: {текст}")
+
+bot.run("ТОКЕН_БОТА")
