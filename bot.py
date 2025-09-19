d730ef50981fe8f56b602c8b585e3b1dd54d4e69
DaniilprogerAI <daniilprogerai@gmail.com>
2025-09-19 11:07:26 +0300

    Create file .env

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,8 +15,8 @@ async def on_ready():
     print(f"Бот {bot.user} запущен и готов!")
 
 @bot.command()
-async def submit(ctx, *, текст):
-    await ctx.send(f"{ctx.author.mention} отправил задачу: {текст}")
+async def submit(ctx, *, text):
+    await ctx.send(f"{ctx.author.mention} отправил задачу: {text}")
 
 TOKEN = os.getenv("BOT_TOKEN")
 bot.run(TOKEN)
